aa5a5efea6489c8c2305d5e2e983e5f87b6e1eb1
4015a11 <4015a11@limon.ece.uprm.edu>
2016-08-31 08:42:00 -0400

    Fixed the frame size, changed window title from "Hello, World!" to "PR Flag".

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,10 +2,10 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
+		JFrame myFrame = new JFrame("PR Flag");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(860, 440);
-		myFrame.setSize(400, 400);
+		myFrame.setSize(300, 200);
 		myFrame.setVisible(true);
 		MyPanelClass myPanel = new MyPanelClass();
         myFrame.getContentPane().add(myPanel);
